55048c72dfb5ed59501450ca88ccbaa79d08f3fb
Talita Silva de Oliveira <litaa.oliveira@gmail.com>
2016-07-23 01:40:47 -0300

    primeiro commit, site finalizado provavelmente terá ajustes

---
+++ index.php
@@ -0,0 +1,154 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+	<meta charset="utf-8">
+	<title>Site</title>
+	<link rel="stylesheet" type="text/css" href="css/style.css">
+</head>
+<body>
+	<div id="header">
+		<section id="top" class="container">
+			<header id="logo" class="four columns">
+				<h1>
+					<a href="index.php">Katinha - bolinhos e doces</a>
+				</h1>
+			</header><!-- /header#logo -->
+			<section id="menu" class="eight columns">
+				<nav>
+					<ul>
+						<li>
+							<a href="#secao-home">Home</a>
+						</li>
+						<li>
+							<a href="#secao-produtos">Produtos</a>
+						</li>
+						<li>
+							<a href="#top-10-receitas">Top 10</a>
+						</li>
+						<li>
+							<a href="#secao-encomendas">Encomendas</a>
+						</li>
+						<li>
+							<a href="#footer">Contato</a>
+						</li>
+					</ul>
+				</nav>
+			</section><!-- /section#menu -->
+		</section><!-- /section#top -->
+		<section id="banner">
+			<section id="mensagem-boasvindas" class="container">
+				<div id="mensagem">
+					<h2>Seja muito bem vindo!</h2>
+					<p>
+						Este é um site apenas informativo mostrando alguns dos bolinhos que fazemos, também trabalhamos com brownies e palhas italianas, mas o foco principal são bolinhos (cupcakes mais simples). Trabalhamos com todos os tipos, e fazemos também bolinhos "fit" sem glutém. O carro chefe é o de cenoura com cobertura de chocolate.
+					</p>
+					<h3><a href="#">Leia Mais</a></h3>
+				</div>
+			</section><!-- /section#mensagem-boasvindas -->
+		</section><!-- /section#banner -->
+	</div><!-- /div#header -->
+	<div id="content" class="row">
+		<section id="secao-produtos" class="container">
+			<header>
+				<h2>Produtos</h2>
+			</header>
+			<div class="row">
+				<section id="content-produtos-1" class="content-produtos">
+					<section class="four columns item-produto">
+						<figure>
+							<img src="fotos/bolinho1.jpeg" alt="Esta imagem exibe uma ilustração com três personagens" />
+						</figure>
+					</section>
+					<section class="four columns item-produto">
+						<h3>01</h3>
+						<p>Sabor da massa: a massa do bolo pode ser a que você desejar: cenoura, chocholate, leite ninho, entre outras...</p>
+					</section>
+					<section class="four columns item-produto">
+						<figure>
+							<img src="fotos/bolinho2.jpeg" alt="Esta imagem exibe uma ilustração com três personagens" />
+						</figure>
+					</section>
+				</section>
+			</div>
+			<div class="row">
+				<section id="content-produtos-2" class="content-produtos">
+					<section class="four columns item-produto">
+						<h3>02</h3>
+						<p>Cobertura: trabalhamos com diversos tipos de coberturas, entre elas: chocolate, brigadeiro, limão, leite ninho, etc..</p>
+					</section>
+					<section class="four columns item-produto">
+						<figure>
+							<img src="fotos/bolinho3.jpeg" alt="Esta imagem exibe uma ilustração com três personagens" />
+						</figure>
+					</section>
+					<section class="four columns item-produto">
+						<h3>03</h3>
+						<p>Topping: pode ser granulados coloridos, dourados, bolinhas, etc..</p>
+					</section>
+				</section>
+			</div>
+		</section><!-- /section#secao-produtos -->
+		<section id="top-10-receitas" class="container">
+			<section id="top-dez">
+				<h2>Top 10 Receitas</h2>
+				<h3>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</h3>
+				<p>Nulla debitis aperiam nihil provident commodi consectetur numquam, quam, consequatur cum inventore tempore nemo velit reprehenderit. Pariatur corporis dolore similique accusantium. Non.</p>
+			</section><!-- /section#top-dez -->
+			<section id="botoes" class="row">
+				<input type="button" name="" value="Ver Todas">
+				<input type="button" name="" value="Download">
+			</section><!-- /section#botoes -->
+		</section><!-- /section#top-10-receitas -->
+		<section id="secao-encomendas">
+			<section id="card-review" class="container">
+				<div class="row">
+					<figure class="five columns">
+						<img src="img/mocinha.png">
+					</figure>
+					<section class="seven columns">
+						<h2>Encomendas de bolinhos</h2>
+						<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sequi consequatur nobis ullam? Odio a voluptatum modi earum enim aperiam iure iste rem quas qui beatae perferendis, alias nostrum quidem reprehenderit.</p>
+					</section>
+					<input type="button" name="" value="Saiba Mais">
+				</div>
+			</section>
+		</section><!-- /section#secao-encomendas -->
+		<section id="varios-bolinhos" class="container">
+			<div class="row">
+				<div class="three columns bolinhos">
+					<figure>
+						<img src="fotos/bolinhosozinho.png">
+					</figure>
+				</div>
+				<div class="three columns bolinhos">
+					<figure>
+						<img src="fotos/bolinhosozinho.png">
+					</figure>
+				</div>
+				<div class="three columns bolinhos">
+					<figure>
+						<img src="fotos/bolinhosozinho.png">
+					</figure>
+				</div>
+				<div class="three columns bolinhos">
+					<figure>
+						<img src="fotos/bolinhosozinho.png">
+					</figure>
+				</div>
+			</div>
+		</section><!-- /section#macarrao -->
+	</div><!-- /div#content -->
+	<div id="footer">
+		<section class="container">
+			<p class="four columns item-footer">2016|Katinha Bolinhos e doces</p>
+			<address class="four columns item-footer">
+				Pernambuco<br>
+				Recife<br>
+				Tamarineira<br>
+				(81) 9 9805-0687
+			</address>
+			<p class="four columns item-footer">Copyright &copy; 2016 Katinha. All rights reserved.</p>
+		</section>
+	</div><!-- /div#footer -->
+</body>
+</html>
